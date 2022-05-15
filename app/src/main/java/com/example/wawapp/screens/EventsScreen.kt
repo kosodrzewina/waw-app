305eb5f9739c87ae35ac7a1d42228c003b2e3e63
Piotr Sienkiewicz <sienkiew@live.com>
2022-05-15 19:56:32 +0200

    selecting category in events screen

--- app/src/main/java/com/example/wawapp/screens/EventsScreen.kt
+++ app/src/main/java/com/example/wawapp/screens/EventsScreen.kt
@@ -1,24 +1,87 @@
 package com.example.wawapp.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.wawapp.Event
+import com.example.wawapp.EventType
 import com.example.wawapp.composables.EventList
 
 @Composable
 fun EventsScreen(events: List<Event>) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    var eventsToDisplay by remember {
+        mutableStateOf(events)
+    }
+    var selectedCategory by remember {
+        mutableStateOf("All")
+    }
+
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Events") })
+            TopAppBar(
+                title = { Text(text = "Events") },
+                actions = {
+                    if (events.isNotEmpty()) {
+                        Text(text = selectedCategory, modifier = Modifier.padding(all = 16.dp))
+                        Box {
+                            IconButton(
+                                onClick = { isExpanded = !isExpanded },
+                                modifier = Modifier.padding(
+                                    top = 16.dp,
+                                    end = 16.dp,
+                                    bottom = 16.dp
+                                )
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.FilterList,
+                                    contentDescription = null
+                                )
+                            }
+                            DropdownMenu(
+                                expanded = isExpanded,
+                                onDismissRequest = { isExpanded = false },
+                            ) {
+                                DropdownMenuItem(onClick = {
+                                    eventsToDisplay = events
+                                    isExpanded = false
+                                    selectedCategory = "All"
+                                }) {
+                                    Text(text = "All")
+                                }
+
+                                EventType.values().forEach { eventType ->
+                                    DropdownMenuItem(onClick = {
+                                        eventsToDisplay = events.filter { event ->
+                                            event.type == eventType.suffix
+                                        }
+                                        isExpanded = false
+                                        selectedCategory = eventType.suffix
+                                    }) {
+                                        Text(text = eventType.suffix)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            )
         },
     ) {
+        if (eventsToDisplay.isEmpty() && events.isNotEmpty()) {
+            // TODO: Some cool screen
+            Text(text = "There's no events in this category")
+        }
         EventList(
-            events = events,
+            events = eventsToDisplay,
             modifier = Modifier
                 .padding(top = it.calculateTopPadding())
                 .fillMaxHeight()
