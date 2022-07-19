1f33a677160f7ba4a048b5902fc4ebb25c332267
Piotr Sienkiewicz <sienkiew@live.com>
2022-07-19 23:32:43 +0200

    nav graph in a separate file

--- app/src/main/java/com/example/wawapp/MainActivity.kt
+++ app/src/main/java/com/example/wawapp/MainActivity.kt
@@ -6,65 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.wawapp.navigation.BottomNavBar
-import com.example.wawapp.navigation.EVENTS_ROUTE
-import com.example.wawapp.navigation.EVENT_PREVIEW_ROUTE
-import com.example.wawapp.navigation.MAP_ROUTE
-import com.example.wawapp.screens.EventPreviewScreen
-import com.example.wawapp.screens.EventsScreen
-import com.example.wawapp.screens.MapScreen
-import java.net.URLDecoder
-import java.nio.charset.StandardCharsets
+import com.example.wawapp.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navHostController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val navController = rememberNavController()
+            navHostController = rememberNavController()
 
-            Scaffold(
-                bottomBar = { BottomNavBar(navController = navController) }
-            ) {
-                NavHost(
-                    navController = navController,
-                    startDestination = EVENTS_ROUTE,
-                    modifier = Modifier.padding(paddingValues = it)
-                ) {
-                    composable(EVENTS_ROUTE) {
-                        EventsScreen(
-                            events = EventStore.events,
-                            navController
-                        )
-                    }
-                    composable(MAP_ROUTE) { MapScreen() }
-                    composable(
-                        route = "$EVENT_PREVIEW_ROUTE/{encodedGuid}",
-                        arguments = listOf(
-                            navArgument("encodedGuid") {
-                                type = NavType.StringType
-                                nullable = false
-                            }
-                        )
-                    ) { entry ->
-                        val decodedGuid = URLDecoder.decode(
-                            entry.arguments?.getString("encodedGuid"),
-                            StandardCharsets.UTF_8.toString()
-                        )
-
-                        EventPreviewScreen(
-                            event = EventStore.events.first { event ->
-                                event.guid == decodedGuid
-                            },
-                            navController = navController
-                        )
-                    }
-                }
+            Scaffold(bottomBar = { BottomNavBar(navController = navHostController) }) {
+                NavGraph(
+                    navController = navHostController,
+                    modifier = Modifier.padding(it)
+                )
             }
         }
     }
---
+++ app/src/main/java/com/example/wawapp/navigation/NavGraph.kt
@@ -0,0 +1,51 @@
+package com.example.wawapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.wawapp.EventStore
+import com.example.wawapp.screens.EventPreviewScreen
+import com.example.wawapp.screens.EventsScreen
+import com.example.wawapp.screens.MapScreen
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
+
+@Composable
+fun NavGraph(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController, startDestination = EVENTS_ROUTE, modifier = modifier
+    ) {
+        composable(EVENTS_ROUTE) {
+            EventsScreen(
+                events = EventStore.events,
+                navController
+            )
+        }
+        composable(MAP_ROUTE) { MapScreen() }
+        composable(
+            route = "$EVENT_PREVIEW_ROUTE/{encodedGuid}",
+            arguments = listOf(
+                navArgument("encodedGuid") {
+                    type = NavType.StringType
+                    nullable = false
+                }
+            )
+        ) { entry ->
+            val decodedGuid = URLDecoder.decode(
+                entry.arguments?.getString("encodedGuid"),
+                StandardCharsets.UTF_8.toString()
+            )
+
+            EventPreviewScreen(
+                event = EventStore.events.first { event ->
+                    event.guid == decodedGuid
+                },
+                navController = navController
+            )
+        }
+    }
+}
