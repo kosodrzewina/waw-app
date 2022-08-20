159706fa7d26cab9ab6360053ec7d15842d6feb2
Piotr Sienkiewicz <sienkiew@live.com>
2022-08-20 14:17:05 +0200

    event title in the top bar (EventPreviewScreen)

--- app/src/main/java/com/example/wawapp/screens/EventPreviewScreen.kt
+++ app/src/main/java/com/example/wawapp/screens/EventPreviewScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavController
 import com.example.wawapp.EventWebView
 import com.example.wawapp.events.Event
@@ -15,7 +16,13 @@ fun EventPreviewScreen(event: Event, navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Event") },
+                title = {
+                    Text(
+                        text = event.title,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 2
+                    )
+                },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
