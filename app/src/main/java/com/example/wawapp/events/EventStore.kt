22c5a4af29dabbd5f0ceb9429c7b7411c6023294
Piotr Sienkiewicz <sienkiew@live.com>
2022-08-20 14:18:18 +0200

    replacing events with freshly fetched ones instead of adding them

--- app/src/main/java/com/example/wawapp/events/EventStore.kt
+++ app/src/main/java/com/example/wawapp/events/EventStore.kt
@@ -3,14 +3,11 @@ package com.example.wawapp.events
 import androidx.compose.runtime.mutableStateListOf
 
 object EventStore {
-    private val EVENTS = mutableStateListOf<Event>()
-    val events: List<Event> get() = EVENTS
+    var events = mutableStateListOf<Event>()
+        private set
 
     fun updateEvents(newEvents: List<Event>) {
-        EVENTS.addAll(newEvents.filter { fetchedEvent ->
-            EVENTS.all { event ->
-                event.guid != fetchedEvent.guid
-            }
-        })
+        events.clear()
+        events.addAll(newEvents)
     }
 }
