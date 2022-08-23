674c08cb428ba16f555ed6084d9f0256d35dd75d
Piotr Sienkiewicz <sienkiew@live.com>
2022-08-23 14:29:33 +0200

    sorting favourite events alphabetically

--- app/src/main/java/com/example/wawapp/events/EventStore.kt
+++ app/src/main/java/com/example/wawapp/events/EventStore.kt
@@ -1,8 +1,20 @@
 package com.example.wawapp.events
 
 import androidx.compose.runtime.mutableStateListOf
+import java.text.Collator
+import java.util.*
+
+const val LANGUAGE = "pl"
+
+private class EventComparator : Comparator<Event> {
+    private val collator = Collator.getInstance(Locale(LANGUAGE))
+
+    override fun compare(p0: Event?, p1: Event?): Int =
+        collator.compare(p0?.eventTitle, p1?.eventTitle)
+}
 
 object EventStore {
+    private val comparator = EventComparator()
     var events = mutableStateListOf<Event>()
         private set
     var favouriteEvents = mutableStateListOf<Event>()
@@ -16,5 +28,6 @@ object EventStore {
     fun updateFavouriteEvents(newEvents: List<Event>) {
         favouriteEvents.clear()
         favouriteEvents.addAll(newEvents)
+        favouriteEvents.sortWith(comparator)
     }
 }
