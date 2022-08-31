6466e52833849e72898f62c12faf634da66baa2f
Piotr Sienkiewicz <sienkiew@live.com>
2022-08-31 14:35:40 +0200

    light mode only

--- app/src/main/java/com/example/wawapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wawapp/ui/theme/Theme.kt
@@ -30,11 +30,7 @@ onSurface = Color.Black,
 
 @Composable
 fun WawAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
